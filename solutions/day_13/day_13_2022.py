80846d9ee1fd8a3bef54b0cfd99e3a62ea65af8d
Laura Twardy <ltwardy@verizon.net>
2022-12-13 19:16:20 -0500

    Day 13 in progress

--- solutions/day_13/day_13_2022.py
+++ solutions/day_13/day_13_2022.py
@@ -31,18 +31,15 @@ def compare_values(left, right):
     print(left, " vs ", right)
 
     if type(left) is int and type(right) is int:
+        # print("compare ints")
         return compare_ints(left, right)
 
     elif type(left) is list and type(right) is list:
-        for e, l_element in enumerate(left):
-            try:
-                r_element = right[e]
-            except IndexError:
-                print("missing right-hand element")
-                return "wrong"
-            return compare_values(l_element, r_element)
+        # print("compare lists")
+        return compare_lists(left, right)
 
     else:  # one list, one integer
+        # print("compare mixed")
         return compare_mixed(left, right)
 
 
@@ -50,14 +47,16 @@ def compare_ints(left: int, right: int):
     """If both arguments are integers, return a string related to their order; if not, raise an error."""
     if type(left) is not int or type(right) is not int:
         raise TypeError(f"Expected integer arguments: {left}, {right}.")
-    print("integers: ", left, " vs ", right)
+    print("Compare integers: ", left, " vs ", right)
     if left < right:
+        print("Left is smaller, so inputs are in the right order.")
         return "correct"
     elif right < left:
+        print("Right is smaller, so inputs ar NOT in the right order.")
         return "wrong"
     else:
         assert right == left
-        print("equal integers")
+        # print("equal integers")
         return "equal"
 
 
@@ -65,25 +64,28 @@ def compare_lists(left: list, right: list):
     """Compare elements in lists index by index; if the right side has nothing to compare, the order is 'wrong'."""
     if type(left) is not list or type(right) is not list:
         raise TypeError(f"Expected list arguments: {left}, {right}.")
-    print("lists: ", left, " vs ", right)
+    print("Compare lists: ", left, " vs ", right)
     for e, l_element in enumerate(left):
         try:
             r_element = right[e]
         except IndexError:
             print("ran out of right-hand elements")
-            return "wrong"
+            order = "wrong"
+            return order
         order = compare_values(l_element, r_element)
         if order == "equal":
             print("equal list elements")
-            continue
+        elif order == "wrong":
+            return "wrong"
         else:
-            return order
+            assert order == "correct"
+            return "correct"
     else:  # if the for loop is exhausted without incident
-        return "correct"
+        return "equal"
 
 
 def compare_mixed(left, right):
-    print("mixed: ", left, " vs ", right)
+    print("Compare mixed: ", left, " vs ", right)
     if type(left) is int:
         assert type(right) is list
         left = [left]
@@ -99,16 +101,19 @@ def solve_part_1(signal):
     print(signal)
     for index, pair in enumerate(signal):
         print(pair)
-        order = compare_lists(pair[0], pair[1])
-        if order == "correct":
+        # order = compare_lists(pair[0], pair[1])
+        order = compare_values(pair[0], pair[1])
+        if order == "correct" or order == "equal":
             print("That pair is in the correct order!")
-            correct_indices.append(index)
+            correct_indices.append(index + 1)
         else:
             print("Wrong order, sorry.")
         print()
     score = sum(correct_indices)
     return score
-
+    # 52-something-something was too high
+    # 4017 is too low
+    # but the test case works, so this is going to require more work.  sigh.
 
 def solve_part_2(input_data):
     """Describe the next puzzle."""
@@ -130,15 +135,15 @@ def solution(filename):
 
 # This can be run as a script from the command line, with data filename as argument.
 
-# if __name__ == "__main__":
-#     import sys
-#
-#     try:
-#         arg = sys.argv[1]
-#     except IndexError:
-#         raise SystemExit(f"Usage: {sys.argv[0]} <data file for this puzzle>")
-#
-#     print(f"Data file = '{arg}'.")  # debug
-#     solution(arg)
+if __name__ == "__main__":
+    import sys
 
-solution("testing1.txt")
+    try:
+        arg = sys.argv[1]
+    except IndexError:
+        raise SystemExit(f"Usage: {sys.argv[0]} <data file for this puzzle>")
+
+    print(f"Data file = '{arg}'.")  # debug
+    solution(arg)
+
+# solution("testing.txt")   # debug
